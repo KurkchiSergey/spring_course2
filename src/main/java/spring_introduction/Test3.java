5dee4e25cbd5ae06184de6925c2795e4320d6d91
5BenGun5 <stanki-ku@yandex.ru>
2026-09-18 14:55:44 +0300

    change commit

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -1,14 +1,14 @@
 package spring_introduction;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test2 {
+public class Test3 {
     public static void main(String[] args) {
+    //    Pet pet = new Dog();
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("aplicationContext.xml");
         Pet pet = context.getBean("myPet", Pet.class);
-        pet.say();
-        context.close();
+        Person person = new Person(pet);
+        person.callPet();
     }
 }
